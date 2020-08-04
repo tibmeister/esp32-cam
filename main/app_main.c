7bd93e072ffd6a4630f2d05754d41b40fb77b4ed
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2020-08-04 14:36:21 -0500

    Added in OTA bits

--- main/app_main.c
+++ main/app_main.c
@@ -19,7 +19,23 @@
 //#include "app_sntp.h"
 #endif
 
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
+#define HASH_LEN 32 /* SHA-256 digest length */
+static const char *TAG = "application_main";
+
 EventGroupHandle_t event_group;
+
+static void print_sha256 (const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i) {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+    ESP_LOGI(TAG, "%s: %s", label, hash_print);
+}
 
 void app_shutdown() {
   app_settings_shutdown();
@@ -39,6 +55,48 @@ void app_shutdown() {
 
 void app_main()
 {
+  uint8_t sha_256[HASH_LEN] = { 0 };
+  esp_partition_t partition;
+
+  // get sha256 digest for the partition table
+  partition.address   = ESP_PARTITION_TABLE_OFFSET;
+  partition.size      = ESP_PARTITION_TABLE_MAX_LEN;
+  partition.type      = ESP_PARTITION_TYPE_DATA;
+  esp_partition_get_sha256(&partition, sha_256);
+  print_sha256(sha_256, "SHA-256 for the partition table: ");
+
+  // get sha256 digest for bootloader
+  partition.address   = ESP_BOOTLOADER_OFFSET;
+  partition.size      = ESP_PARTITION_TABLE_OFFSET;
+  partition.type      = ESP_PARTITION_TYPE_APP;
+  esp_partition_get_sha256(&partition, sha_256);
+  print_sha256(sha_256, "SHA-256 for bootloader: ");
+
+  // get sha256 digest for running partition
+  esp_partition_get_sha256(esp_ota_get_running_partition(), sha_256);
+  print_sha256(sha_256, "SHA-256 for current firmware: ");
+
+  // If we've gotten this far it's far to assume we are good to go
+  const esp_partition_t *running = esp_ota_get_running_partition();
+  esp_ota_img_states_t ota_state;
+
+  if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK)
+  {
+    if(ota_state == ESP_OTA_IMG_PENDING_VERIFY)
+    {
+        ESP_LOGI(TAG, "Update was successful, switching to run this going forward");
+        esp_err_t errRb = esp_ota_mark_app_valid_cancel_rollback();
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Update was not sucessful, rolling back the firmware and rebooting");
+        esp_err_t errRb = esp_ota_mark_app_invalid_rollback_and_reboot();
+    }
+  }
+
+  // Startup pause so things can be read
+  vTaskDelay(10000 / portTICK_PERIOD_MS);
+
   EventBits_t uxBits;
 
   ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -50,27 +108,37 @@ void app_main()
 
   app_camera_startup();
   #ifdef CONFIG_LED_ILLUMINATOR_ENABLED
-  app_illuminator_startup();
+    app_illuminator_startup();
   #endif
   app_wifi_startup();
 
   for (;;) {
 	  uxBits = xEventGroupWaitBits(event_group,WIFI_CONNECTED_BIT | WIFI_SOFTAP_BIT,pdFALSE,pdFALSE,500 / portTICK_PERIOD_MS);
+
 	  if (uxBits > 0) {
       #ifdef CONFIG_SNTP_ENABLED
-      //app_sntp_startup();
+        //app_sntp_startup();
       #endif
+
       #ifdef CONFIG_MDNS_ENABLED
-      ESP_ERROR_CHECK(mdns_init());
-      ESP_ERROR_CHECK(mdns_hostname_set(settings.hostname));
-      ESP_ERROR_CHECK(mdns_instance_name_set(settings.mdns_instance));
-      mdns_txt_item_t serviceTxtData[1] = { {"path","/"} };
-      ESP_ERROR_CHECK( mdns_service_add(settings.mdns_instance, "_http", "_tcp", 80, serviceTxtData, 1) );
+        ESP_ERROR_CHECK(mdns_init());
+        ESP_ERROR_CHECK(mdns_hostname_set(settings.hostname));
+        ESP_ERROR_CHECK(mdns_instance_name_set(settings.mdns_instance));
+        mdns_txt_item_t serviceTxtData[1] = { {"path","/"} };
+        ESP_ERROR_CHECK( mdns_service_add(settings.mdns_instance, "_http", "_tcp", 80, serviceTxtData, 1) );
       #endif
+
       app_httpd_startup();
+
       #ifdef CONFIG_USE_SSD1306_LCD_DRIVER
-      app_lcd_startup();
+        app_lcd_startup();
       #endif
+
+      #ifdef CONFIG_OTA_ENABLED
+        //Startup the OTA process
+        ota_init();
+      #endif
+
       return;
     }
   }
